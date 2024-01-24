f700f1ca431bedccc1f8dd882cbc0c7217c8b35b
PamelaSesante <112668433+PamelaSesante@users.noreply.github.com>
2024-01-24 14:48:15 +0800

    Update IndexNotFoundException.java

---
+++ IndexNotFoundException.java
@@ -0,0 +1,11 @@
+package phantasianbistrodemo;
+
+public class IndexNotFoundException extends Exception{
+
+    public IndexNotFoundException() {
+    }
+
+    public IndexNotFoundException(String msg) {
+        super(msg);
+    }
+}
