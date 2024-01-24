f16b748413760de017f9a79a45c91fcbe624ab1c
PamelaSesante <112668433+PamelaSesante@users.noreply.github.com>
2024-01-24 14:52:11 +0800

    Update UnsuitableGiftException.java

---
+++ UnsuitableGiftException.java
@@ -0,0 +1,12 @@
+
+package phantasianbistrodemo;
+
+public class UnsuitableGiftException extends Exception {
+
+    public UnsuitableGiftException() {
+    }
+
+    public UnsuitableGiftException(String msg) {
+        super(msg);
+    }
+}
